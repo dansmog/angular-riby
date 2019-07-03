73da5d9c360204e59f72e7d26e0d3aac3fb7be85
dansmog <juwonanthony@gmail.com>
2019-07-03 22:03:26 +0100

    Add home, discovery and bookmarks component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { DiscoveryComponent } from './discovery/discovery.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { PortfolioComponent } from './portfolio/portfolio.component'
+
+
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent,
+  },
+  {
+    path: 'discovery', component: DiscoveryComponent
+  },
+  {
+    path: 'bookmarks', component: BookmarksComponent
+  },
+  {
+    path: 'portfolio', component: PortfolioComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DiscoveryComponent } from './discovery/discovery.component';
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DiscoveryComponent,
+    AuthModalComponent,
+    BookmarksComponent,
+    PortfolioComponent,
   ],
   imports: [
     BrowserModule,
