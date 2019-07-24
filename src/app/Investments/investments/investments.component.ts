0896c0a717f43465ce7630e54f8b3e8b2ed18b6f
dansmog <juwonanthony@gmail.com>
2019-07-24 18:50:48 +0100

    add search to investment page

--- src/app/Investments/investments/investments.component.ts
+++ src/app/Investments/investments/investments.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from "ngx-spinner";
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 
 import { RestApiService } from 'src/app/shared/rest-api.service';
@@ -14,6 +15,8 @@ export class InvestmentsComponent implements OnInit {
   total = null;
   currentPage = 1;
   isLoading: boolean = true;
+  queryParams: string = ''
+  isSearchLoading: boolean = false;
 
   constructor(private rest: RestApiService, private spinner: NgxSpinnerService) { }
 
@@ -29,6 +32,15 @@ export class InvestmentsComponent implements OnInit {
       this.isLoading = false;
       this.spinner.hide()
       console.log(this.investments)
+    })
+  }
+
+  searchForInvestments(type: string) {
+    this.isSearchLoading = true;
+    this.rest.filterResults(this.queryParams, type).pipe(debounceTime(100), distinctUntilChanged()).subscribe(data => {
+      this.isSearchLoading = false;
+      this.total = data.payload.total;
+      this.investments = data.payload.contribution_types;
     })
   }
 
